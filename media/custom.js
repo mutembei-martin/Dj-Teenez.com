3a7e389086dd27873eccc49322aa30b8f03e3467
mutembei-martin <walkerrigby@gmail.com>
2020-10-31 00:18:59 +0300

    first

---
+++ media/custom.js
@@ -0,0 +1,23 @@
+
+/* HTML document is loaded. DOM is ready. 
+-------------------------------------------*/
+
+
+  // HOME BACKGROUND SLIDESHOW
+  $(function(){
+    jQuery(document).ready(function() {
+
+
+    $('#body').backstretch([
+       "images/12.png", 
+       "images/8.png",
+       "images/9.png",
+        "images/17.png",
+        ],  {duration: 10000, fade: 2000});
+
+    });
+
+  })
+
+
+
