67c4b26783b3f7c7c321179d93fb8554b4c3be58
mutembei-martin <walkerrigby@gmail.com>
2020-12-02 11:41:31 +0300

    The Launch

--- media/custom.js
+++ media/custom.js
@@ -4,23 +4,7 @@
 
 
   // HOME BACKGROUND SLIDESHOW
-  $(function(){
-    jQuery(document).ready(function() {
 
-
-    $('body').backstretch([ 
-       "media/13.jpg", 
-        "media/15.jpg",
-       "media/2.jpg",
-       "media/1.jpg",
-        "media/7.jpg",
-        "media/6.jpg"
-        ],  {duration: 15000, fade: 2000});
-
-
-    });
-
-  });
 
 
 
